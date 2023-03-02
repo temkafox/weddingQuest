0d6e72910b19c60158b522ec06c8f8e590e2027a
Artem Suchilin <temkafox@mail.ru>
2023-03-02 16:21:07 +0300

    new questions added

--- src/data.ts
+++ src/data.ts
@@ -207,4 +207,186 @@ export const data = [
       },
     ]
   },
+  {
+    questionNumber: 9,
+    questionText: 'Что Артём спрятал в киндер сюрпризе для Саши?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Кольцо',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Серьги',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Подвеску',
+        isTrue: true,
+      },
+      {
+        id: 4,
+        text: 'Цепочку',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 10,
+    questionText: 'Самое необычное признание в любви Артему от Саши?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'В бассейне',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Головоломкой',
+        isTrue: true,
+      },
+      {
+        id: 3,
+        text: 'Пазлами',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Кроссвордом',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 11,
+    questionText: 'Что Артём любит на завтрак больше всего?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Блины',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Глазунью',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Чокопай',
+        isTrue: true,
+      },
+      {
+        id: 4,
+        text: 'Круассаны',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 12,
+    questionText: 'Какому девизу Саша учит Артему на отдыхе ?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Отоспаться за все отработанное время',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'На отдыхе можно выкидывать грязную одежду, а не стирать',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Кто рано встает, тому Бог подает',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Тратим все что накопили на отдых',
+        isTrue: true,
+      },
+    ]
+  },
+  {
+    questionNumber: 13,
+    questionText: 'Любимый цвет одежды Артема до встречи с Сашей ?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Синий',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Черный',
+        isTrue: true,
+      },
+      {
+        id: 3,
+        text: 'Белый',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Зеленый',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 14,
+    questionText: 'Какой предмет одежды Саша дарила чаще всего?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Шорты',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Носки',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Трусы',
+        isTrue: true,
+      },
+      {
+        id: 4,
+        text: 'Шарфы',
+        isTrue: false,
+      },
+    ]
+  },
+  {
+    questionNumber: 15,
+    questionText: 'В виде какого зверя подаренная Артемом брошь Саше?',
+    questionAnswers: [
+      {
+        id: 1,
+        text: 'Кошка',
+        isTrue: false,
+      },
+      {
+        id: 2,
+        text: 'Медведь',
+        isTrue: false,
+      },
+      {
+        id: 3,
+        text: 'Собака',
+        isTrue: false,
+      },
+      {
+        id: 4,
+        text: 'Лиса',
+        isTrue: true,
+      },
+    ]
+  },
 ]
